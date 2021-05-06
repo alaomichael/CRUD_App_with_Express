4fcdc9d0e6cfbe4b7339ca45afaa4bf979695f9d
Michael Alao <contactleomax@gmail.com>
2021-05-06 12:34:15 +0100

    Updated database URL

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 // Connect database 
+const URL = 'mongodb+srv://alaomichael:babatunde4@myfirstcluster.ctklt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const mongoose = require('mongoose');
 mongoose.connect('URL', () => {
    console.log('Connected to Mongo DB Successfully!!');
