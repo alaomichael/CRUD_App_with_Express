d8c8fd9a6ef68bba206a8f150c558577fe4dc2dd
Michael Alao <contactleomax@gmail.com>
2021-05-06 12:23:27 +0100

    Connect local database

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CRUD_App_with_Express
 Created with CodeSandbox
+
+You are also required to create a simple documentation of your routes in your github readme file
+
+You are to submit your your github link with the hosted link in the readme file too
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-let http = require("http")
-const express = require("express")
+const express = require('express')
 
+// Connect database 
+const mongoose = require('mongoose');
+mongoose.connect('URL', () => {
+   console.log('Connected to Mongo DB Successfully!!');
+})
 
 //create a server object:
 var app = express();
 app.listen(3000, () => {
-   console.log(‘Server listening on 3000’);
+   console.log(`Server listening on 3000`);
 })
